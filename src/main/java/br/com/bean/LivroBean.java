1e54f97aa97739073ff3f329cb8a7545e89426b1
LuizdosReis <luizdosreis@gmail.com>
2017-03-04 21:32:45 -0300

    adicionando a inserção de autores na view e no bean

--- src/main/java/br/com/bean/LivroBean.java
+++ src/main/java/br/com/bean/LivroBean.java
@@ -1,39 +1,61 @@
 package br.com.bean;
 
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
 
 import br.com.dao.DAO;
+import br.com.model.Autor;
 import br.com.model.Livro;
 
 @ManagedBean
-public class LivroBean{
+@ViewScoped
+public class LivroBean {
 
 	private Livro livro = new Livro();
-	
+	private Integer autorId;
+
+	public Integer getAutorId() {
+		return autorId;
+	}
+
+	public void setAutorId(Integer autorId) {
+		this.autorId = autorId;
+	}
 
 	public Livro getLivro() {
 		return livro;
 	}
 
-
 	public void setLivro(Livro livro) {
 		this.livro = livro;
 	}
 
+	public List<Autor> getAutores() {
+		return new DAO<Autor>(Autor.class).listaTodos();
+	}
+	
+	public List<Autor> getAutoresDoLivro() {
+		return livro.getAutores();
+	}
+	
+	public void gravarAutor() {
+		Autor autor = new DAO<Autor>(Autor.class).buscoPorId(autorId);
+		livro.adicionaAutor(autor);
+	}
+	
+	public void gravar() {
+		System.out.println("livro gravado" + livro.getTitulo());
 
-	public void gravar(){
-		System.out.println("livro gravado"+livro.getTitulo());
-		
 		if (livro.getAutores().isEmpty()) {
 			throw new RuntimeException("Livro deve ter pelo menos um Autor.");
 		}
-		
+
 		new DAO<Livro>(Livro.class).adiciona(livro);
-		
+
 		livro = new Livro();
-		
+
 	}
-	
-	
-	
+
 }
